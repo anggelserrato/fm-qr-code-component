6a1f18cdb09e5efe68e6312d3e0ff603b1ea6c21
Angel Serrato <serrato.anggel@gmail.com>
2026-03-24 10:23:55 -0500

    feat: integrate prettier-plugin-tailwindcss for consistent class ordering

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,19 @@ import image from './assets/image-qr-code.png';
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="flex min-h-screen flex-col">
       <article className="flex flex-1 items-center justify-center">
-        <figure className="bg-white rounded-2xl p-sm flex flex-col gap-md max-w-xs shadow-lg">
+        <figure className="flex max-w-xs flex-col gap-md rounded-2xl bg-white p-sm shadow-lg">
           <img
             src={image}
             alt="QR code para visitar Frontend Mentor"
-            className="rounded-xl w-full"
+            className="w-full rounded-xl"
           />
-          <figcaption className="flex flex-col gap-sm px-sm mb-md">
-            <h1 className="text-[22px] font-bold leading-[120%] tracking-normal text-slate-900 text-center">
+          <figcaption className="mb-md flex flex-col gap-sm px-sm">
+            <h1 className="text-center text-[22px] leading-[120%] font-bold tracking-normal text-slate-900">
               Improve your front-end skills by building projects
             </h1>
-            <p className="text-[15px] text-slate-500 text-center leading-[140%] tracking-[0.2px]">
+            <p className="text-center text-[15px] leading-[140%] tracking-[0.2px] text-slate-500">
               Scan the QR code to visit Frontend Mentor and take your coding
               skills to the next level
             </p>
@@ -22,14 +22,14 @@ function App() {
         </figure>
       </article>
 
-      <footer className="text-center pb-4 text-body text-slate-500">
+      <footer className="text-body pb-4 text-center text-slate-500">
         Challenge by{' '}
         <a
           href="https://www.frontendmentor.io?ref=challenge"
           target="_blank"
           rel="noopener noreferrer"
           aria-label="Frontend Mentor (opens in a new tab)"
-          className="text-slate-900 font-bold"
+          className="font-bold text-slate-900"
         >
           Frontend Mentor
         </a>
@@ -39,7 +39,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
           aria-label="Angel Serrato (opens in a new tab)"
-          className="text-slate-900 font-bold"
+          className="font-bold text-slate-900"
         >
           Angel Serrato
         </a>
